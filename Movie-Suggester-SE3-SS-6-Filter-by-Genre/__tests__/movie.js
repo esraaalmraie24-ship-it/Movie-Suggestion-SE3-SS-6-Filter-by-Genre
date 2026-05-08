80664ba443e9d30afaab90ee89b0209fdfd3f1fe
esraaalmraie24-ship-it <esraaalmraie24@gmail.com>
2026-05-09 00:50:44 +0300

    Update movie.js

--- Movie-Suggester-SE3-SS-6-Filter-by-Genre/__tests__/movie.js
+++ Movie-Suggester-SE3-SS-6-Filter-by-Genre/__tests__/movie.js
@@ -1,27 +1,3 @@
-// دالة لتصفية الأفلام حسب النوع (Genre)
-function filterByGenre(movies, genre) {
-    
-    if (!movies || !Array.isArray(movies)) {
-        return [];
-    }
-    
-    // فلتر الأفلام اللي نوعها يساوي genre المطلوب
-    return movies.filter(movie => movie.genre === genre);
-}
-
-// دالة لجلب كل أنواع الأفلام (بدون تكرار)
-function getAllGenres(movies) {
-    if (!movies || !Array.isArray(movies)) {
-        return [];
-    }
-    
-    const genres = movies.map(movie => movie.genre);
-    return [...new Set(genres)];
-}
-
-// تصدير الدوال للاستخدام في الاختبارات
-module.exports = {
-    filterByGenre,
-    getAllGenres
-};
-// test
+test('simple test', () => {
+    expect(true).toBe(true);
+});
